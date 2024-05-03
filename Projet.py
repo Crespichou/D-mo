3fa04a18ce57162ffa7c9f2bbbfe05810ca76d78
Antoine <antoine.crespinee@gmail.com>
2024-05-03 17:07:13 +0200

    Interface

--- Projet.py
+++ Projet.py
@@ -2,3 +2,10 @@ class Sleevy :
     def __init__(self, n_serie) :
         self.n_serie = n_serie
 
+class Interface(Sleevy) :
+    def __init__(self, username, password) :
+        self.username = username
+        self.password = password
+
+    def log_in(self) : 
+        print(f"To connect, please enter your {self.username} and your {self.password}")
