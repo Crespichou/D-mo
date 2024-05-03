e88bac7172cb25b8e21ef6ca4f60f6980fadc9ba
Antoine <antoine.crespinee@gmail.com>
2024-05-03 17:33:28 +0200

    Main

--- Projet.py
+++ Projet.py
@@ -9,3 +9,38 @@ class Interface(Sleevy) :
 
     def log_in(self) : 
         print(f"To connect, please enter your {self.username} and your {self.password}")
+class Sensor(Sleevy) :
+     def __init__(self, type, position, data_type) :
+         self.type = type
+         self.position = position
+         self.data_type = data_type
+
+class PPG(Sensor) :
+    def get_info(self) :
+        print(f"I'm the {self.type} sensor set up in {self.position} in the {self.n_serie} Sleevy")
+    
+    def task(self) :
+        print(f"I'm sending {self.data_type} data")
+
+
+class EMG(Sensor) :
+    def get_info(self) :
+        print(f"I'm the {self.type} sensor set up in {self.position}")
+
+    def task(self) :
+        print(f"I'm sending {self.data_type} data")
+
+class Accel(Sensor) :
+    def get_info(self) :
+        print(f"I'm the {self.type} sensor set up in {self.position}")
+    
+    def task(self) :
+        print(f"I'm sending {self.data_type} data")
+
+
+class Heat(Sensor) :
+    def get_info(self) :
+        print(f"I'm the {self.type} sensor set up in {self.position}")
+
+    def task(self) :
+        print(f"I'm sending {self.data_type} data")
