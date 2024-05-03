fe3146153521b81b5387a25aa5598e3c54f25a66
Antoine <antoine.crespinee@gmail.com>
2024-05-03 17:22:56 +0200

    Sensor

--- Projet.py
+++ Projet.py
@@ -31,9 +31,3 @@ class Accel(Sensor) :
         print(f"I'm sending {self.data_type} data")
 
 
-class Heat(Sensor) :
-    def get_info(self) :
-        print(f"I'm the {self.type} sensor set up in {self.position}")
-
-    def task(self) :
-        print(f"I'm sending {self.data_type} data")
